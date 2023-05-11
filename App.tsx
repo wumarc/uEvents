c8e24e837f7478676ae31c41df9a024de87a6f28
Marc WU <32423253+wumarc@users.noreply.github.com>
2023-05-10 23:36:26 -0400

    adding icons to navbar

--- App.tsx
+++ App.tsx
@@ -6,6 +6,8 @@ import SignedupEventsPage from "./src/components/pages/SignedupEventsPage";
 import SearchPage from "./src/components/pages/SearchPage";
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
 
 const Tab = createMaterialBottomTabNavigator();
 
@@ -14,11 +16,56 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Events" component={SignedupEventsPage} />
-        <Tab.Screen name="Saved" component={SavedEventsPage} />
-        <Tab.Screen name="Home" component={HomePage} />
-        <Tab.Screen name="Search" component={SearchPage} />
-        <Tab.Screen name="Profile" component={ProfilePage} />
+        <Tab.Screen 
+          name="Events" 
+          component={SignedupEventsPage}
+          options={{
+            tabBarLabel: 'Events',
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="calendar" color={color} size={26} />
+            ),
+          }}
+        />
+        <Tab.Screen 
+          name="Saved" 
+          component={SavedEventsPage}
+          options={{
+            tabBarLabel: 'Saved',
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="calendar-heart" color={color} size={26} />
+            ),
+          }}
+        />
+        <Tab.Screen 
+          name="Home" 
+          component={HomePage} 
+          options={{
+            tabBarLabel: 'Home',
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="home" color={color} size={26} />
+            ),
+          }}
+        />
+        <Tab.Screen 
+          name="Search" 
+          component={SearchPage}
+          options={{
+            tabBarLabel: 'Search',
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="magnify" color={color} size={26} />
+            ),
+          }}
+        />
+        <Tab.Screen 
+          name="Profile" 
+          component={ProfilePage}
+          options={{
+            tabBarLabel: 'Profile',
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="account-circle" color={color} size={26} />
+            ),
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
