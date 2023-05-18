59dc8398408e6ff73d0e27ec45cd575f9e51cb7f
Marc WU <32423253+wumarc@users.noreply.github.com>
2023-05-17 20:47:01 -0400

    added scrollview to savedEventsPage

--- src/screens/SavedEventsPage.tsx
+++ src/screens/SavedEventsPage.tsx
@@ -1,13 +1,18 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import Event from "../components/organisms/Event";
 import { mockEventClimbing } from "../utils/model/EventObject";
 
 const SavedEventsPage = () => {
   return (
-    <View>
+    <ScrollView
+      style={{
+        height: "100%",
+        width: "100%",
+      }}
+    >
       <Text>Your Saved Events</Text>
       <Event event={mockEventClimbing} />
-    </View>
+    </ScrollView>
   );
 };
 
