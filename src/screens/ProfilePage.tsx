2622880b03951b038dbe53c8fa2a84813585afec
Marc WU <32423253+wumarc@users.noreply.github.com>
2023-05-19 23:32:22 -0400

    adding profile section without styles

--- src/screens/ProfilePage.tsx
+++ src/screens/ProfilePage.tsx
@@ -1,13 +1,64 @@
 import { View, Text} from "react-native"
+import { Button, Input } from '@rneui/themed';
 
 const ProfileScreen = () => {
 
     return (
         <View>
-            <Text>Profile Screen</Text>
+            {/* Header Section */}
+            <View style={{ flexDirection: "row" }}>
+                <Text>Your Profile</Text>
+            </View>
+
+            {/* Image Section */}
+            <View style={{ flexDirection: "row" }}>
+                <Text>Image</Text>
+            </View>
+            {/* <View style={{ flexDirection: "column" }}> */}
+
+            {/* Student Info Section */}
+            <View style={{ flexDirection: "row" }}>
+                <View style={{ flexDirection: "column" }}>
+                    <Input
+                        placeholder='Student Name'
+                        leftIcon={{ type: 'font-awesome', name: 'user' }}
+                    />
+                    <Input
+                        placeholder='Student ID'
+                        leftIcon={{ type: 'font-awesome', name: 'id-card' }}
+                    />
+                    <Input
+                        placeholder='Student Email'
+                        leftIcon={{ type: 'font-awesome', name: 'envelope' }}
+                    />
+                    <Input
+                        placeholder='Password'
+                        leftIcon={{ type: 'font-awesome', name: 'lock' }}
+                    />
+                    <Input
+                        placeholder='Phone Number'
+                        leftIcon={{ type: 'font-awesome', name: 'phone' }}
+                    />
+                </View>
+            </View>
+
+            {/* Save Changes Section */}
+            <View style={{ flexDirection: "row" }}>
+                <Button 
+                    color="primary"
+                    radius="lg"
+                    size="md"
+                >
+                    Save Changes
+                </Button>
+            </View>
+
         </View>
     )
 
 }
+
+
+
 
 export default ProfileScreen
