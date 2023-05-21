e4dbf26f3d012b2fc8fc95f1485750bf33499aaf
Marc WU <32423253+wumarc@users.noreply.github.com>
2023-05-21 11:18:52 -0400

    app navigation refactoring

--- App.tsx
+++ App.tsx
@@ -8,84 +8,115 @@ import { NavigationContainer, useNavigation } from "@react-navigation/native";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import { SafeAreaView, StyleSheet } from "react-native";
 import { Platform } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import EventDetails from "./src/components/organisms/EventDetails";
 // import 'react-native-gesture-handler';
 
 const Tab = createMaterialBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+const MainView = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Events"
+        component={SignedupEventsPage}
+        options={{
+          tabBarLabel: "Events",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="calendar"
+              color={color}
+              size={26}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Saved"
+        component={SavedEventsPage}
+        options={{
+          tabBarLabel: "Saved",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="calendar-heart"
+              color={color}
+              size={26}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Home"
+        component={HomePage}
+        options={{
+          tabBarLabel: "Home",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={SearchPage}
+        options={{
+          tabBarLabel: "Search",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="magnify"
+              color={color}
+              size={26}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfilePage}
+        options={{
+          tabBarLabel: "Profile",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="account-circle"
+              color={color}
+              size={26}
+            />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+const DetailedView = () => {
+  return (
+    <EventDetails/>
+  );
+};
 
 export default function App() {
 
   return (
     <NavigationContainer>
+
       <SafeAreaView style={styles.container}>
-        <Tab.Navigator>
-          <Tab.Screen
-            name="Events"
-            component={SignedupEventsPage}
+        <Stack.Navigator initialRouteName="MainView">  
+          {/* Main View */}
+          <Stack.Screen
+            name="MainView"
+            component={MainView}
             options={{
-              tabBarLabel: "Events",
-              tabBarIcon: ({ color }) => (
-                <MaterialCommunityIcons
-                  name="calendar"
-                  color={color}
-                  size={26}
-                />
-              ),
+              headerShown: false
             }}
           />
-          <Tab.Screen
-            name="Saved"
-            component={SavedEventsPage}
-            options={{
-              tabBarLabel: "Saved",
-              tabBarIcon: ({ color }) => (
-                <MaterialCommunityIcons
-                  name="calendar-heart"
-                  color={color}
-                  size={26}
-                />
-              ),
-            }}
+          {/* Any other view that adds a stack to the main view, we only have detailedView for events */}
+          <Stack.Screen 
+            name="DetailedView"
+            component={DetailedView}
           />
-          <Tab.Screen
-            name="Home"
-            component={HomePage}
-            options={{
-              tabBarLabel: "Home",
-              tabBarIcon: ({ color }) => (
-                <MaterialCommunityIcons name="home" color={color} size={26} />
-              ),
-            }}
-          />
-          <Tab.Screen
-            name="Search"
-            component={SearchPage}
-            options={{
-              tabBarLabel: "Search",
-              tabBarIcon: ({ color }) => (
-                <MaterialCommunityIcons
-                  name="magnify"
-                  color={color}
-                  size={26}
-                />
-              ),
-            }}
-          />
-          <Tab.Screen
-            name="Profile"
-            component={ProfilePage}
-            options={{
-              tabBarLabel: "Profile",
-              tabBarIcon: ({ color }) => (
-                <MaterialCommunityIcons
-                  name="account-circle"
-                  color={color}
-                  size={26}
-                />
-              ),
-            }}
-          />
-        </Tab.Navigator>
+        </Stack.Navigator>
       </SafeAreaView>
+
     </NavigationContainer>
   );
 }
--- src/screens/SignedupEventsPage.tsx
+++ src/screens/SignedupEventsPage.tsx
@@ -3,11 +3,11 @@ import Event from "../components/organisms/Event";
 import { mockEventClimbing } from "../utils/model/EventObject";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import EventDetails from "../components/organisms/EventDetails";;
-import { Button } from "@rneui/base";
 
 let placeholders = ['1', '1', '1', '1', '1']
 
-const HomeView = ({navigation}: {navigation: any}) => {
+const SignedupEventsPage = ({navigation}: any) => {
+  
   return (
     <ScrollView
       style={{
@@ -23,7 +23,7 @@ const HomeView = ({navigation}: {navigation: any}) => {
         return (
           <TouchableOpacity
             key={index}
-            onPress={() => {navigation.navigate('DetailsView')}}
+            onPress={() => {navigation.navigate('DetailedView')}}
           >
               <Event
                 key={index}
@@ -34,35 +34,6 @@ const HomeView = ({navigation}: {navigation: any}) => {
       })}
 
     </ScrollView>
-  )
-}
-
-const DetailsView = ({navigation}: {navigation: any}) => {
-  return (
-    <View>
-      <EventDetails/>
-    </View>
-  )
-}
-
-const Stack = createNativeStackNavigator();
-
-const SignedupEventsPage = () => {
-  
-  return (
-    <Stack.Navigator initialRouteName="HomeView">
-      <Stack.Screen 
-        name="HomeView" 
-        component={HomeView}
-        options={{
-          headerShown: false
-        }}
-      />
-      <Stack.Screen 
-        name="DetailsView" 
-        component={DetailsView}
-      />
-    </Stack.Navigator>
   );
 
 };
