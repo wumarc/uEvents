2f50f658f7ddfe9d940afcc11d64be0e33122aed
Marc WU <32423253+wumarc@users.noreply.github.com>
2023-05-22 21:24:27 -0400

    adding spacing file

---
+++ src/components/subatoms/Spacing.tsx
@@ -0,0 +1,17 @@
+
+
+export const ScreenSpacing = {
+    marginHorizontal: 20,
+    marginVertical: 20,
+    paddingHorizontal: 20,
+    paddingVertical: 20,
+}
+
+export const EventSpacing = {
+    marginHorizontal: 10,
+    marginVertical: 10,
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+}
+
+// Add more stylings...
