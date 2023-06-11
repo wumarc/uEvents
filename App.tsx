c41b48233a617e6e840cde7ec7172a3f4e5411d1
Marc WU <32423253+wumarc@users.noreply.github.com>
2023-06-11 00:07:27 -0400

    adding splash screen

--- App.tsx
+++ App.tsx
@@ -4,9 +4,26 @@ import SignIn from "./src/components/pages/SignIn";
 import Main from "./src/main";
 import { Loading } from "./src/components/pages/LoadingPage";
 import { View, Text } from "react-native";
+import { useState } from "react";
+import LottieView from 'lottie-react-native';
+import { StyleSheet } from "react-native";
 
 export default function App() {
   const [user, loading, error] = useAuthState(auth);
+  const [splashLoaded, setSplashLoaded] = useState(false);
+
+  if(!splashLoaded) {
+    return (
+      <View style={styles.splash}>
+        <LottieView
+          source={require('./assets/splash.json')}
+          autoPlay
+          loop={false}
+          onAnimationFinish={() => setSplashLoaded(true)}
+        />
+      </View>
+    )
+  }
 
   if (loading) {
     return <Loading />;
@@ -23,3 +40,11 @@ export default function App() {
     return <SignIn />;
   }
 }
+
+const styles = StyleSheet.create({
+  splash: {
+    flex: 1,
+    alighItems: 'center',
+    margin: 0
+  }
+})
