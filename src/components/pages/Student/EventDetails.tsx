74176840c19068f2be5bc9f884357ef15ca4d1f3
Marc WU <32423253+wumarc@users.noreply.github.com>
2023-07-13 22:59:55 -0400

    EventDetails Revamp,util func Event,added maps

--- src/components/pages/Student/EventDetails.tsx
+++ src/components/pages/Student/EventDetails.tsx
@@ -1,21 +1,19 @@
 import {
   View,
   Text,
-  Image,
   StyleSheet,
   ScrollView,
   ActivityIndicator,
   Linking,
   ImageBackground,
 } from "react-native";
-import { EventObject } from "../../../utils/model/EventObject";
-import { Button } from "react-native-elements";
+import { EventObject, extractMonth, extractDay, extractDayOfWeek } from "../../../utils/model/EventObject";
+import { Button, Icon } from "react-native-elements";
 import { colours } from "../../subatoms/colours";
 import { Subtitle, regularText } from "../../subatoms/Theme";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParamList } from "./main";
 import { useStateWithFireStoreDocument } from "../../../utils/useStateWithFirebase";
-import { StatusBar } from "react-native";
 import DateCard from "../../atoms/DateCard";
 import { LinearGradient } from "expo-linear-gradient";
 
@@ -45,7 +43,7 @@ const EventDetails = ({ route, navigation }: props) => {
 
           {/* Event Image */}
           <ImageBackground
-            style={{ width: "100%", height: "100%" }}
+            style={{ width: "100%", height: "20%" }}
             source={{
               uri: "https://i.pinimg.com/originals/a2/cd/2c/a2cd2c7e4383e563cc6a65489968b5af.jpg",
             }}
@@ -79,48 +77,66 @@ const EventDetails = ({ route, navigation }: props) => {
             <View>
 
               {/* Event date and on/off-campus */}
-              <View style={{flexDirection: 'row', marginVertical: '2%'}}>
+              <View style={{flexDirection: 'row', marginVertical: '2%', justifyContent: 'space-between'}}>
                 
                 <View style={{ flexDirection: "row", width: '70%'}}>
                   <DateCard
-                    line1={"29"} 
-                    line2={"December"}
+                    line1={extractDay(event.time)}
+                    line2={extractMonth(event.time)}
                     style={{width: '34%'}}
                   />
                   <Text>{"    "}</Text>
                   <DateCard
-                    line1={"Tuesday"} 
-                    line2={"10:00 PM - 11:00 PM"}
+                    line1={extractDayOfWeek(event.time)} 
+                    line2={"TBD"}
                     style={{width: '34%'}}
                   />
                 </View>
 
-                <View style={styles.onOffCampus}>
-                  <Text style={{color: 'white', fontWeight: '600'}}>{event.onCampus == true ? "On-Campus": "Off-Campus"}</Text>
+                <View style={{
+                  ...styles.price,
+                  backgroundColor: event.price > 0 ? colours.primaryPurple : 'green',
+                }}>
+                  <Text style={{
+                    color: 'white',
+                    fontWeight: '600',
+                    fontSize: 20,
+                  }}>
+                    {event.price > 0 ? '$'+event.price : 'Free'}
+                  </Text>
                 </View>
 
               </View>
 
               {/* Location and Address */}
-              <View style={{marginVertical: '1%'}}>
+              <View style={{marginVertical: '1%', flexDirection: 'row', justifyContent: 'space-between'}}>
                 <View>
-                  <Text style={{
-                    color: 'white', 
-                    fontWeight: 'bold', 
-                    paddingVertical: '0.5%', 
-                    fontSize: 17
-                  }}>
-                      {event.location}
-                  </Text>
+                  <View>
+                    <Text style={{ color: 'white', fontWeight: 'bold', paddingVertical: '0.5%', fontSize: 17}}>
+                        {event.location}
+                    </Text>
+                  </View>
+                  <View>
+                    <Text style={{color: '#e3e3e3', fontWeight: '400'}}>
+                        {event.address}
+                    </Text>
+                  </View>
                 </View>
-                <View>
-                  <Text style={{
-                    color: '#e3e3e3',
-                    fontWeight: '400',
-                  }}>
-                      {event.address}
-                  </Text>
+                
+                <View style={{justifyContent:'center', alignItems: 'center'}}>
+                    <Button
+                      type="clear"
+                      icon={{
+                        name: "external-link",
+                        type: "font-awesome",
+                        size: 22,
+                        color: "white",
+                      }}
+                      containerStyle= {{ backgroundColor: 'none' }}
+                      onPress={() => {Linking.openURL("https://www.google.com/maps/search/?api=1&query=" + event.address)}}
+                    />                  
                 </View>
+
               </View>
 
               {/* Event Description */}
@@ -147,11 +163,65 @@ const EventDetails = ({ route, navigation }: props) => {
                 </View>
               </View>
 
-              {/* Other Details */}
-              <View>
-                <View></View>
-                <View></View>
+              {/* Food */}
+              {event.food && (
+                <View style={{marginVertical: '2%'}}>
+                <View>
+                  <Text style={{fontSize: 20, fontWeight: "600", flexWrap: "wrap", color: "white", paddingBottom: '1%'}}>Food</Text>
+                </View>
+                <View>
+                  <Text style={{color: '#e3e3e3', fontWeight: '400', fontSize: 17}}>{event.food}</Text>
+                </View>
               </View>
+              )}
+
+              {/* Attire */}
+              {event.attire && (
+                <View style={{marginVertical: '2%'}}>
+                <View>
+                  <Text style={{fontSize: 20, fontWeight: "600", flexWrap: "wrap", color: "white", paddingBottom: '1%'}}>Attire</Text>
+                </View>
+                <View>
+                  <Text style={{color: '#e3e3e3', fontWeight: '400', fontSize: 17}}>{event.attire}</Text>
+                </View>
+              </View>
+              )}
+
+              {/* toBring */}
+              {event.toBring && (
+                <View style={{marginVertical: '2%'}}>
+                <View>
+                  <Text style={{fontSize: 20, fontWeight: "600", flexWrap: "wrap", color: "white", paddingBottom: '1%'}}>What to Bring</Text>
+                </View>
+                <View>
+                  <Text style={{color: '#e3e3e3', fontWeight: '400', fontSize: 17}}>{event.toBring}</Text>
+                </View>
+              </View>
+              )}
+
+              {/* Includes */}
+              {event.includes && (
+                <View style={{marginVertical: '2%'}}>
+                <View>
+                  <Text style={{fontSize: 20, fontWeight: "600", flexWrap: "wrap", color: "white", paddingBottom: '1%'}}>What's Included</Text>
+                </View>
+                <View>
+                  <Text style={{color: '#e3e3e3', fontWeight: '400', fontSize: 17}}>{event.includes}</Text>
+                </View>
+              </View>
+              )}
+
+              {/* Transportation */}
+              {event.transportation && (
+                <View style={{marginVertical: '2%'}}>
+                <View>
+                  <Text style={{fontSize: 20, fontWeight: "600", flexWrap: "wrap", color: "white", paddingBottom: '1%'}}>Transportation</Text>
+                </View>
+                <View>
+                  <Text style={{color: '#e3e3e3', fontWeight: '400', fontSize: 17}}>{event.transportation}</Text>
+                </View>
+              </View>
+              )}
 
             </View>
           </LinearGradient>
@@ -161,16 +231,22 @@ const EventDetails = ({ route, navigation }: props) => {
 
       {/* Footer */}
       <View style={styles.footer}>
-        <Text style={styles.price}>{event.price > 0 ? '$'+event.price : 'Free'}</Text>
+        <Text style={{
+          color: 'white', 
+          fontWeight: '600', 
+          fontSize: 19
+        }}>
+          {event.onCampus == true ? "On-Campus": "Off-Campus"}
+        </Text>
         <Button
           buttonStyle={{
             backgroundColor: colours.primaryPurple,
             padding: 15,
             borderRadius: 15,
           }}
-          title="View"
+          title= {event.signUpLink == null ? "No Sign Up Required" : "Sign Up"}
           icon={{
-            name: "external-link",
+            name: event.signUpLink == null  ? "" : "external-link",
             type: "font-awesome",
             size: 15,
             color: "white",
@@ -181,7 +257,7 @@ const EventDetails = ({ route, navigation }: props) => {
           //   marginVertical: 10,
           // }}
           onPress={() => {
-            Linking.openURL("https://www.google.com");
+            Linking.openURL(event.signUpLink!);
             // navigation.navigate("EventSignUpView", {
             //   userType: route.params.userType,
             // });
@@ -208,23 +284,17 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
     paddingVertical: 10,
-    paddingHorizontal: 5,
+    paddingHorizontal: '2.3%',
     alignItems: "center",
     // backgroundColor: colours.secondaryGrey,
     backgroundColor: colours.secondaryPurple,
   },
   price: {
-    fontSize: 20,
-    fontWeight: "bold",
-    color: "white",
-  },
-  onOffCampus: {
     backgroundColor: colours.primaryPurple,
     borderRadius: 25,
-    width: '30%', 
-    justifyContent: 'center', 
+    width: '25%',
+    justifyContent: 'center',
     alignItems: 'center',
-    
   }
 });
 
