0ffe00456d1fc381d1bffea9d73581d20724d056
Marc WU <32423253+wumarc@users.noreply.github.com>
2023-08-14 19:27:11 -0400

    create event form change

--- src/components/pages/EventOrganizer/CreateEvent.tsx
+++ src/components/pages/EventOrganizer/CreateEvent.tsx
@@ -1,5 +1,6 @@
 import { View, Text, ScrollView, StyleSheet, Modal, KeyboardAvoidingView, Platform, Pressable} from "react-native"
-import { Icon, Input } from "react-native-elements"
+import { Icon } from "react-native-elements"
+import { Input } from "@rneui/themed";
 import { useState } from "react";
 import { Button } from "@rneui/base";
 import { colours, fonts, spacing, windowHeight, windowWidth } from "../../subatoms/Theme";
@@ -7,11 +8,10 @@ import { ProgressBar } from "react-native-paper";
 import {Calendar, LocaleConfig} from 'react-native-calendars';
 import { Dropdown } from 'react-native-element-dropdown';
 import emojiRegex from 'emoji-regex';
-import { Image } from '@rneui/themed';
 
 export const Step0 = ({route, navigation}: any) => {
     
-    const [step, setStep] = useState(0);
+    const [step, setStep] = useState(1);
 
     return (
         <KeyboardAvoidingView
@@ -22,7 +22,7 @@ export const Step0 = ({route, navigation}: any) => {
             {/* Render the right step dynamically */}
             <ScrollView>
                 <View style={{paddingHorizontal: spacing.page2, ...spacing.verticalPadding1}}>
-                    {step == 0 && <StepWelcome />}
+                    {/* {step == 0 && <StepWelcome />} */}
                     {step == 1 && <Step1 />}
                     {step == 2 && <Step2 />}
                     {step == 3 && <Step3 />}
@@ -33,29 +33,30 @@ export const Step0 = ({route, navigation}: any) => {
                     {step == 8 && <Step8 />}
                     {step == 9 && <Step9 />}
                     {step == 10 && <Step10 />}
+                    {step == 11 && <Step11 />}
                 </View>
             </ScrollView>
 
             {/* Static Footer */}
             <View style={{marginBottom: windowHeight*0.01}}>
                 <ProgressBar
-                    progress={(step+1)*0.1}
+                    progress={step*0.09}
                     color={colours.purple}
                 />
                 <View style={styles.footer_buttons}>
                     <Button
                         buttonStyle={{backgroundColor: colours.white}}
                         title={"Back"}
-                        onPress={() => step==0 ? navigation.pop() : setStep(step -1)}
+                        onPress={() => step==1 ? navigation.pop() : setStep(step -1)}
                         titleStyle={{...fonts.title3, textDecorationLine: 'underline'}}
                         disabledStyle={{backgroundColor: colours.white}}
                         disabledTitleStyle={{color: colours.white}}
-                        disabled={step==9}
+                        disabled={step==11}
                     />
                     <Button
                         buttonStyle={{backgroundColor: colours.purple, padding: 15, paddingHorizontal: 25, borderRadius: 10}}
-                        title={step != 9 ? "Next" : "Finish"}
-                        onPress={() => {step != 10 ? setStep(step + 1) : navigation.pop()}}
+                        title={step != 11 ? "Next" : "Finish"}
+                        onPress={() => {step != 11 ? setStep(step + 1) : navigation.pop()}}
                         titleStyle={{...fonts.title2, color: colours.white}}
                     />
                 </View>
@@ -116,6 +117,49 @@ export const Step1 = ({route, navigation}: any) => {
 }
 
 export const Step2 = ({route, navigation}: any) => {
+    
+    const [unicodePath, setUnicodePath] = useState<string>('../../../assets/openmojis/1F600.png');
+
+    // use this to valide the emoji
+    const isEmoji = (character: string) => {
+        const regex = emojiRegex();
+        return regex.test(character);
+    };
+
+    function emojiToUnicode(emoji: any) {
+        if(emoji.length == 0) return;
+        setUnicodePath('../../../assets/openmojis/'+ emoji.codePointAt(0).toString(16).toUpperCase());
+    }
+
+    return (
+        <View>
+            <Text style={{...fonts.title1, ...spacing.verticalMargin1}}>Pick an emoji to represent your event</Text>
+            <Text style={fonts.regular}>Who still uses images? Yuck! Emojis are cooler 😎</Text>
+
+            <View style={{marginVertical: '5%', paddingHorizontal: '15%'}}>
+                <Input
+                    selectionColor={colours.black}
+                    autoCapitalize="none"
+                    inputContainerStyle={{
+                        borderColor: colours.grey,
+                        borderBottomWidth: 1,
+                        borderWidth: 1,
+                        paddingVertical: 4,
+                        paddingHorizontal: 10,
+                        borderRadius: 6,
+                        height: windowHeight*0.2
+                    }}
+                    inputStyle={{fontSize: 150, fontWeight: 'bold'}}
+                    onChange={(e) => {emojiToUnicode(e.nativeEvent.text)}}
+                    maxLength={5}
+                />
+                
+            </View>
+        </View>
+    )
+}
+
+export const Step3 = ({route, navigation}: any) => {
 
     const [nextStep, setNextStep] = useState<boolean>(false);
     const [onCampus, setOnCampus] = useState<boolean>(false);
@@ -152,7 +196,7 @@ export const Step2 = ({route, navigation}: any) => {
                 <View>
                     <Text style={fonts.title1}>{onCampus ? "In which building is your event taking place?" : "Where will the event take place?"}</Text>
                     <View>
-                        <Text>{onCampus ? "Dropdown" : "Please provide the building name and then the address"}</Text>
+                        <Text>{onCampus ? "Dropdown" : "Provide the building name and then the address"}</Text>
 
                         {/* Dropdown */}
                         <Dropdown
@@ -185,7 +229,7 @@ export const Step2 = ({route, navigation}: any) => {
     )
 }
 
-export const Step3 = ({route, navigation}: any) => {
+export const Step4 = ({route, navigation}: any) => {
     
     const [selected, setSelected] = useState('');
     const [modalVisible, setModalVisible] = useState(false);
@@ -206,7 +250,7 @@ export const Step3 = ({route, navigation}: any) => {
 
     return (
         <View>
-            <Text style={fonts.title1}>Please provide the date and time of your event</Text>
+            <Text style={fonts.title1}>Provide the date and time of your event</Text>
 
             <View>
                 <Text>From:</Text>
@@ -266,50 +310,53 @@ export const Step3 = ({route, navigation}: any) => {
 
 }
 
-export const Step4 = ({route, navigation}: any) => {
+export const Step5 = ({route, navigation}: any) => {
 
     const [charactersAvailable, setCharactersAvailable] = useState<number>(200);
 
     return (
         <View>
             <View>
-                <Text style={fonts.title1}>Provide a description of your event</Text>
-                <Text style={fonts.regular}>Share more details about your event.</Text>
+                <Text style={{...fonts.title1, ...spacing.verticalMargin1}}>Provide a description of your event</Text>
+                <Text style={fonts.regular}>Anything you would like to share that is not in the title.</Text>
             </View>
 
-            <Text>{charactersAvailable} <Text style={{color: colours.grey}}>characters available</Text></Text>
-            <Input
-                selectionColor={colours.black}
-                inputStyle={{height: windowHeight*0.13}}
-                inputContainerStyle={{
-                    borderColor: colours.grey,
-                    borderWidth: 1,
-                    paddingVertical: 4,
-                    paddingHorizontal: 10,
-                    borderRadius: 6,
-                }}
-                textAlignVertical= 'top'
-                multiline={true}
-                containerStyle={{paddingHorizontal: 0}}
-                onChange={(e) => {setCharactersAvailable(200 - e.nativeEvent.text.length)}}
-                maxLength={200}
-            />
+            <View style={{marginVertical: '5%'}}>
+                <Text>{charactersAvailable} <Text style={{color: colours.grey}}>characters available</Text></Text>
+                <Input
+                    selectionColor={colours.black}
+                    inputStyle={{height: windowHeight*0.13}}
+                    inputContainerStyle={{
+                        borderColor: colours.grey,
+                        borderWidth: 1,
+                        paddingVertical: 4,
+                        paddingHorizontal: 10,
+                        borderRadius: 6,
+                    }}
+                    textAlignVertical= 'top'
+                    multiline={true}
+                    containerStyle={{paddingHorizontal: 0}}
+                    onChange={(e) => {setCharactersAvailable(200 - e.nativeEvent.text.length)}}
+                    maxLength={200}
+                />
+            </View>
+            
 
         </View>
     )
 }
 
-export const Step5 = ({route, navigation}: any) => {
+export const Step6 = ({route, navigation}: any) => {
     return (
         <View>
             
             <View>
-                <Text style={fonts.title1}>Now, set your price</Text>
+                <Text style={{...fonts.title1, ...spacing.verticalMargin1}}>Now, set your price</Text>
                 <Text style={fonts.regular}>If your event is free, just leave it blank</Text>
             </View>
 
             {/* Price */}
-            <View>
+            <View style={{marginVertical: '5%'}}>
                 <Input
                     selectionColor={colours.black}
                     autoCapitalize="none"
@@ -334,33 +381,51 @@ export const Step5 = ({route, navigation}: any) => {
     )
 }
 
-export const Step6 = ({route, navigation}: any) => {
+export const Step7 = ({route, navigation}: any) => {
     return (
             
         <View>
-            <Text style={fonts.title1}>If your event requires sign up, please provide the link</Text>
-            <Text style={fonts.regular}>Skip this step if sign up is not required.</Text>
+            
+            <Text style={{...fonts.title1, ...spacing.verticalMargin1}}>Provide a link to sign up</Text>
+            <Text style={fonts.regular}>Skip this step if your event requires no sign up.</Text>
         </View>
 
 
 
-    )
-}
-
-export const Step7 = ({route, navigation}: any) => {
-    return (
-        <View>
-            <Text style={fonts.title1}>Recurrence</Text>
-        </View>
     )
 }
 
 export const Step8 = ({route, navigation}: any) => {
     return (
+        <View>
+            
+            <View>
+                <Text style={{...fonts.title1, ...spacing.verticalMargin1}}>Tags</Text>
+                <Text style={fonts.regular}>Pick up to 5 tags to represent your event</Text>                
+            </View>
+
+            <View>
+
+            </View>
+
+        </View>
+    )
+}
+
+export const Step9= ({route, navigation}: any) => {
+    return (
+        <View>
+            <Text style={{...fonts.title1, ...spacing.verticalMargin1}}>Recurrence</Text>
+        </View>
+    )
+}
+
+export const Step10 = ({route, navigation}: any) => {
+    return (
             <View>
                 
                 <View>
-                    <Text style={fonts.title1}>Review and publish</Text>
+                    <Text style={{...fonts.title1, ...spacing.verticalMargin1}}>Review and publish</Text>
                     <Text style={fonts.regular}>Let's review your event to confirm all the details</Text>
                 </View>
 
@@ -410,65 +475,19 @@ export const Step8 = ({route, navigation}: any) => {
     )
 }
 
-export const Step9 = ({route, navigation}: any) => {
+export const Step11 = ({route, navigation}: any) => {
     return (
         <View>
-            <Text style={fonts.title1}>🎉 You're all set! A member of the uEvents team will approve your event very shortly. 🎉</Text>
-
-            <Text style={fonts.regular}>You can view your event on the Events page.</Text>
-        </View>
-    )
-}
-
-export const Step10 = ({route, navigation}: any) => {
-    
-    const [unicodePath, setUnicodePath] = useState<string>('../../../assets/openmojis/1F600.png');
-
-    // use this to valide the emoji
-    const isEmoji = (character: string) => {
-        const regex = emojiRegex();
-        return regex.test(character);
-    };
-
-    function emojiToUnicode(emoji: any) {
-        if(emoji.length == 0) return;
-        setUnicodePath('../../../assets/openmojis/'+ emoji.codePointAt(0).toString(16).toUpperCase());
-    }
-
-    return (
-        <View>
-            <Text style={fonts.title1}>Pick an emoji to represent your event</Text>
-            <Text style={fonts.regular}>Who still uses images? Yuck! Emojis are cooler 😎</Text>
-
+            
             <View>
-                <Input
-                    selectionColor={colours.black}
-                    autoCapitalize="none"
-                    inputContainerStyle={{
-                        borderColor: colours.grey,
-                        borderBottomWidth: 1,
-                        borderWidth: 1,
-                        paddingVertical: 4,
-                        paddingHorizontal: 10,
-                        borderRadius: 6,
-                        width: windowWidth*0.3,
-                        height: windowHeight*0.1,
-                    }}
-                    inputStyle={{fontSize: 50, fontWeight: 'bold'}}
-                    containerStyle={{padding: 20, justifyContent: 'center', alignItems: 'center'}}
-                    onChange={(e) => {emojiToUnicode(e.nativeEvent.text)}}
-                    maxLength={5}
-                />
-                <Image
-                    // source={require('../../../assets/openmojis/1F600.png')}
-                    source={require('../../../assets/openmojis/1F600.png')}
-                    style={{width: 100, height: 100}}
-                />
-                
+                <Text style={{...fonts.title1, ...spacing.verticalMargin1}}>🎉 You're all set! Sit back and relax while we approve your event. 🎉</Text>
+                <Text style={fonts.regular}>You can view your event on the Events page.</Text>
             </View>
+
         </View>
     )
 }
+
 
 const styles = StyleSheet.create({
     footer_buttons: {
