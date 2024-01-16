e19fcf6b9260ac29a82ab91e8b894fef73b6247e
Antoine Lavigne <63273266+antoine2304@users.noreply.github.com>
2024-01-16 13:19:08 -0500

    Adding userConfig to git

---
+++ userConfig.tsx
@@ -0,0 +1,12 @@
+// This file is intended for configuration
+// Personal config. Global config in is config.tsx
+
+// This is a list of special users that uses test events
+// export const testUsersEvents = ["alavi091@uottawa.ca", "admin@uevents.org"];
+export const testUsersEvents = [];
+
+// List of users that uses test version
+// Uses the path "version-test" in the database
+export const testUsersVersion = [];
+
+export const betaUsers = [];
