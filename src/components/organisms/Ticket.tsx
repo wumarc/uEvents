292675287f0d102bad8f33abb5a4164a0abcea3e
Marc WU <32423253+wumarc@users.noreply.github.com>
2024-01-26 17:08:17 -0500

    change

---
+++ src/components/organisms/Ticket.tsx
@@ -0,0 +1,10 @@
+import { Platform, View } from "react-native";
+import { Text } from "react-native-elements";
+
+export const Ticket = () => {
+
+    <View>
+        <Text>Ticket</Text>
+    </View>
+
+}
--- src/components/pages/Common/TicketsPage.tsx
+++ src/components/pages/Common/TicketsPage.tsx
@@ -1,10 +1,21 @@
-import { View } from "react-native";
+import { Platform, View } from "react-native";
 import { Text } from "react-native-elements";
+import { styles } from "./Home";
+import { colours, fonts, spacing, windowWidth } from "../../subatoms/Theme";
+import EventDetails from "./EventDetails";
 
 export const TicketsPage = () => {
   return (
-    <View>
-      <Text>Tickets Page</Text>
+    <View style={styles.container}>
+      
+      <View style={{padding: '3%'}}>
+        <Text style={fonts.title1}>Tickets</Text>
+      </View>
+
+      <View style={{ alignItems: Platform.OS == "web" ? "flex-start" : "center", ...spacing.verticalMargin1 }}>
+          <EventDetails/>
+      </View>
+
     </View>
   );
 };
